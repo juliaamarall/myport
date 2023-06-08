fc3306953f024afd587c2ecb207071439816cc37
Julia Amaral <105997355+juliaamarall@users.noreply.github.com>
2023-06-08 20:36:50 -0300

    commit01

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.sr = ScrollReveal({reset: true});
+// sr.reveal('.bloco03' , {duration: 2000});
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.sr = ScrollReveal({reset: true});
+    sr.reveal('.proj', {duration: 3000});
+    sr.reveal('#bloco01', {duration: 3000});
+  });
+  
